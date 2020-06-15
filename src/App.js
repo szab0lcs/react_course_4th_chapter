816f12eba56184c5a81348a98027d3012d0820d2
szab0lcs <szabolcskencse@gmail.com>
2020-06-15 16:35:58 +0300

    Edit parameter live

--- src/App.js
+++ src/App.js
@@ -10,22 +10,31 @@ class App extends Component {
      ],
      showPersons: false
    }
-   deletePersonHandler = (personIndex) => {
+
+   nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    })
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    // const person = Object.assign({}, this.state.persons[personIndex]); -- ALTERNATIVE
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons})
+  }
+
+  deletePersonHandler = (personIndex) => {
     //  const persons = this.state.persons.slice(); - ALTERNATIVE
     const persons = [...this.state.persons];
      persons.splice(personIndex, 1);
      this.setState({persons: persons});
    }
 
-   nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-      {name: 'Max', age: 28},
-      {name: event.target.value, age: 25},
-      {name: 'Jack', age: 22}
-    ]
-    })
-   }
    togglePersonsHandler = () => {
     const doesShow =this.state.showPersons;
     this.setState({showPersons: !doesShow});
@@ -51,7 +60,10 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)} 
               name={person.name} 
               age={person.age}
-              key={person.id}/>
+              key={person.id}
+              changed={(event) => {
+                return this.nameChangedHandler(event, person.id);
+              }}/>
           })}
         </div>
       );
